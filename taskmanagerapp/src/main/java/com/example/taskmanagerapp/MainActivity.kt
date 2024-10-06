55011142ab93142e1a58eed22e2ceea2062cf261
TedMoon99 <mtj1015@naver.com>
2024-10-07 00:35:04 +0900

    TaskManagerApp

--- taskmanagerapp/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ taskmanagerapp/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.taskmanagerapp
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmanagerapp.ui.theme.Compose_bacic_practiceTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +36,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Compose_bacic_practiceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                    ) {
+                    TaskManagerApp()
                 }
             }
         }
@@ -31,17 +48,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun TaskManagerApp() {
+    TaskManagerCard(
+        imagePainter = painterResource(id = R.drawable.ic_task_completed),
+        taskStateMessage = stringResource(R.string.taskStateMessage),
+        cheerMessage = stringResource(R.string.cheerMessage),
     )
+}
+
+@Composable
+fun TaskManagerCard(imagePainter: Painter, taskStateMessage: String, cheerMessage: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier= modifier.fillMaxSize()
+    ) {
+        Image(painter = imagePainter, contentDescription = null, alignment = Alignment.Center, contentScale = ContentScale.Crop)
+
+        Text(text = taskStateMessage, fontWeight = FontWeight.Bold, modifier= Modifier.padding(top = 24.dp, bottom = 8.dp))
+
+        Text(text = cheerMessage, fontSize = 16.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_bacic_practiceTheme {
-        Greeting("Android")
+        TaskManagerApp()
     }
 }
